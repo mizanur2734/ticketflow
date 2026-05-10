b02953901b1a590d9bb41ce4cd4ae808ba5294d7
mizanur2734 <mizanurbb2791@gmail.com>
2026-05-10 15:19:25 +0600

    height added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,16 +6,16 @@ const Banner = ({ inProgress, resolved }) => {
     <div className="flex flex-col md:flex-row gap-6 md:gap-16 mt-6 md:mt-12 w-full">
 
       {/* LEFT CARD */}
-      <div className="relative h-44 md:h-56 flex justify-center items-center rounded-xl bg-gradient-to-r from-[#632EE3] to-[#9F62F2] overflow-hidden shadow-lg">
+      <div className="relative w-full h-44 md:h-56 flex justify-center items-center rounded-xl bg-gradient-to-r from-[#632EE3] to-[#9F62F2] overflow-hidden shadow-lg">
 
         <img
           src={vectorimage1}
-          className="absolute left-0 bottom-0 w-56 md:w-72 opacity-70"
+          className="absolute left-0 bottom-0 md:w-72 opacity-70"
           alt=""
         />
         <img
           src={vectorimage1}
-          className="absolute right-0 top-0 w-56 md:w-72 scale-x-[-1] opacity-70"
+          className="absolute right-0 top-0 md:w-72 scale-x-[-1] opacity-70"
           alt=""
         />
 
@@ -26,7 +26,7 @@ const Banner = ({ inProgress, resolved }) => {
       </div>
 
       {/* RIGHT CARD */}
-      <div className="relative h-44 md:h-56 flex justify-center items-center rounded-xl bg-gradient-to-r from-[#54CF68] to-[#00827A] overflow-hidden shadow-lg">
+      <div className="relative w-full h-44 md:h-56 flex justify-center items-center rounded-xl bg-gradient-to-r from-[#54CF68] to-[#00827A] overflow-hidden shadow-lg">
 
         <img
           src={vectorimage1}
